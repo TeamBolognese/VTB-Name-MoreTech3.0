407a24f348350f20c02ae3187d86207e36ffc0d6
Reworr <47640060+keworr@users.noreply.github.com>
2021-10-10 02:28:19 +0300

    Добавлен эндпоинт + валидация ввода, рефакторинг
    Добавлен чекер токенов, валидация, рефакторинг resp переменных

--- app.py
+++ app.py
@@ -28,22 +28,26 @@ def show_index():
     '''
     The front "index" page
     '''
+    response.add_header('Access-Control-Allow-Origin', '*')
     return '''
 <head><title>Hack API</title></head><h2>Hackathon API</h2>
 <h3><br>Endpoints:</h3>
 <b>/api/hello</b> - Hello world (<a href=\"https://vtbmt3.teambolognese.ru/api/hello\">Link</a>)
-<br><b>/login</b> - Authenticate form (<a href=\"https://vtbmt3.teambolognese.ru/login\">Link</a>)
-<br><b>/register</b> - Registration form (<a href=\"https://vtbmt3.teambolognese.ru/register\">Link</a>)
-<br><b>/api/get/users</b> - Get users table (<a href=\"https://vtbmt3.teambolognese.ru/api/get/users\">Link</a>)
-<br><b>/api/login</b> and <b>/api/register</b> - Login/registration API (POST)
-<br><br><br><h3>Database structure:</h3>
-CREATE TABLE types_tree (type_tree_id INTEGER PRIMARY KEY, name TEXT);
+<br><b>/login</b> - Форма авторизации (<a href=\"https://vtbmt3.teambolognese.ru/login\">Link</a>)
+<br><b>/register</b> - Форма регистрации (<a href=\"https://vtbmt3.teambolognese.ru/register\">Link</a>)
+<br><b>/api/get/users</b> - Получение таблицы пользователей из БД (<a href=\"https://vtbmt3.teambolognese.ru/api/get/users\">Link</a>)
+<br><b>/api/token_check/&lt;token&gt;</b> - Проверка токена (возвращает юзернейм владельца токена или False)
+<br><b>/api/login</b> and <b>/api/register</b> - Логин/регистрация API (POST)
+<br><br><br><h3>Структура БД:</h3>
+TABLE types_tree (type_tree_id INTEGER PRIMARY KEY, name TEXT);
 <br>
-CREATE TABLE tree (tree_id INTEGER PRIMARY KEY, health_level INTEGER, type_tree_id INTEGER, FOREIGN KEY(type_tree_id) REFERENCES types_tree(type_tree_id));</b>
+TABLE tree (tree_id INTEGER PRIMARY KEY, health_level INTEGER, type_tree_id INTEGER, FOREIGN KEY(type_tree_id) REFERENCES types_tree(type_tree_id));</b>
 <br>
-CREATE TABLE types_risk (type_risk_id INTEGER PRIMARY KEY, name TEXT);
+TABLE types_risk (type_risk_id INTEGER PRIMARY KEY, name TEXT);
 <br>
-CREATE TABLE users (user_id INTEGER PRIMARY KEY, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL, gender INTEGER, age INTEGER, fullname TEXT, type_risk_id INTEGER, tree_id INTEGER, water INTEGER, money INTEGER, FOREIGN KEY(tree_id) REFERENCES tree(tree_id), FOREIGN KEY(type_risk_id) REFERENCES types_risk(type_risk_id));
+TABLE users (user_id INTEGER PRIMARY KEY, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL, gender INTEGER, age INTEGER, fullname TEXT, type_risk_id INTEGER, tree_id INTEGER, water INTEGER, money INTEGER, FOREIGN KEY(tree_id) REFERENCES tree(tree_id), FOREIGN KEY(type_risk_id) REFERENCES types_risk(type_risk_id));
+<br>
+TABLE tokens (token_id INTEGER PRIMARY KEY, user_id INTEGER NOT NULL, token TEXT NOT NULL UNIQUE, FOREIGN KEY(user_id) REFERENCES users(user_id));
 '''
 
 @app.route('/api/hello')
@@ -68,9 +72,9 @@ def do_register():
     if(username.isalnum()):
         # Валидация ввода
         # TODO: переписать на prepared statements
-        resp = cursor.execute("INSERT INTO users (username, password) VALUES ('" + username + "','" + password +"');")
+        resp = cursor.execute("INSERT INTO users (username, password) VALUES ('" + username + "','" + password +"');").fetchall()
         connect.commit()
-        if (len(resp.fetchall()) > 0):
+        if (len(resp) > 0):
             return "{\"result\":\"false\"}"
         else:
             return "{\"result\":\"true\"}"
@@ -94,9 +98,12 @@ def do_login():
     if(username.isalnum()):
         # Валидация ввода
         # TODO: переписать на prepared statements
-        resp = cursor.execute("SELECT user_id FROM users WHERE username='" + username + "' AND password='" + password + "';")
-        if (len(resp.fetchall()) == 1):
-            return "{\"result\":\"true\",\"token\":\"" + token_hex() + "\"}"
+        resp = cursor.execute("SELECT user_id FROM users WHERE username='" + username + "' AND password='" + password + "';").fetchall()
+        if (len(resp) == 1):
+            token = token_hex()
+            cursor.execute("INSERT INTO tokens (user_id, token) VALUES ('" + str(resp[0][0]) + "','" + token + "');")
+            connect.commit()
+            return "{\"result\":\"true\",\"token\":\"" + token + "\"}"
         else:
             return bottle.HTTPResponse(status=403)
     else:
@@ -106,6 +113,20 @@ def do_login():
 def users():
     resp = cursor.execute("SELECT * FROM users").fetchall()
     return str(resp)
+
+@app.route("/api/token_check/<text>")
+def token_check(text):
+    if(text.isalnum()):
+        # Валидация ввода
+        # TODO: переписать на prepared statements
+        resp = cursor.execute("SELECT user_id FROM tokens WHERE token='" +  text + "';").fetchall()
+        if(len(resp) == 1):
+            username = cursor.execute("SELECT username FROM users WHERE user_id=" + str(resp[0][0]) + ";").fetchall()[0][0]
+            return "{\"result\":\"True\",\"username\":\"" + username + "\"}"
+        else:
+            return "{\"result\":\"False\"}"
+    else:
+        return "{\"result\":\"bad_characters\"}"
 
 if __name__ == '__main__':
     bottle.run(app=StripPathMiddleware(app),
